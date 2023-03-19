9352a5627894be2b4e48faa41e522015560db5f1
alicbm <abarandica1234@gmail.com>
2023-03-19 17:16:41 -0500

    main ad in home page ready for all views

--- src/Carousel/index.tsx
+++ src/Carousel/index.tsx
@@ -4,11 +4,9 @@ import './Carousel.css'
 export const Carousel = () => {
   return (
     <div className='Carousel'>
-      <div className='Carousel-img'>
-        
-      </div>
+      <div className='Carousel-img'></div>
       <div className='Carousel-letter'>
-        <p>Free shipping anywhere in the world.</p>
+        <p>FREE SHIPPING ANYWHERE IN THE WORLD.</p>
       </div>
     </div>
   )
